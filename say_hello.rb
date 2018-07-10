4c41f65c8c2e59c54d2fdb616db1c5b0e25c929f
Kira Hartness <kirahartness@gmail.com>
2018-07-10 15:59:01 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,6 @@
 # Build your say_hello method here
 your_name = "kira"
-def say_hello(your_name, my_name = "jessie")
-  puts "Hello #{name}, I'm #{my_name}"
+def say_hello(your_name="ruby programmer")
+  puts "Hello #{your_name}"
 end
-say_hello ("Julie")
+# say_hello ("Julie")
